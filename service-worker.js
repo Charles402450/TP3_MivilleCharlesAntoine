944eb23d29db34fb75c8e3f6337abd11f1b7226d
Charles402450 <charlesantoinemiville@gmail.com>
2026-09-14 22:44:25 -0400

    changement du cache_name

--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,7 @@
    sinon les anciennes versions continuent d'être servies.
    ================================================================== */
 
-const CACHE_NAME = "chatoyant-v1";
+const CACHE_NAME = "chatoyant-v2";
 
 // Le CDN Tailwind est traité à part : s'il est indisponible au moment de
 // l'installation, addAll() rejetterait tout le précache et le service
